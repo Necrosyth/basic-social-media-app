17829765bcf3a50ed4cd80e8e66bd332a5df3d70
necro <acoustify69420@gmail.com>
2024-01-05 00:15:19 +0530

    add comment button added

--- social/src/components/Comments.jsx
+++ social/src/components/Comments.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {collection, addDoc,query,onSnapshot,orderBy} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { getAuth } from 'firebase/auth'  
 
-const Comments = () => {
+const Comments = (id) => {
   return (
-    <div>Comments</div>
+    <>
+      <div className="container my-3">
+    <form>
+
+  <div className="mb-3">
+    {/* <label htmlFor="exampleInputEmail1" className="form-label">Email address</label> */}
+    <input  type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+
+  <button type="submit" className="btn btn-primary">Add  Comment</button>
+</form>
+      </div>
+    </>
   )
 }
 
--- social/src/components/Post_Details.jsx
+++ social/src/components/Post_Details.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase.config";
+import Comments from "./Comments";
 
 const Post_Details = () => {
   const { id } = useParams();
@@ -33,7 +34,11 @@ const Post_Details = () => {
           <div className="post_usename">
             <h3>{data.author}</h3>
           </div>
+          <div className="post_usename">
+            <h3>{data.description}</h3>
+          </div>
         </div>
+        <Comments postId={id}/>
       </div>
     </>
   );
